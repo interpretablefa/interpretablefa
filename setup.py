af3ed2aadc7204e49dc2d8ac1ebc1d4490955641
Justin Tuazon <142733277+jptuazon@users.noreply.github.com>
2024-11-15 19:13:00 +0800

    Updated version number to 1.1.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(working_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="interpretablefa",
-    version="1.1.0",
+    version="1.1.1",
     author="Justin Philip Tuazon, Gia Mizrane Abubo",
     description="A package for interpretable factor analysis",
     long_description=long_description,
