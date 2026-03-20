5683213e68e0cb3ef191ea3aa27295e8fb8f6fb2
Justin Tuazon <142733277+jptuazon@users.noreply.github.com>
2026-03-20 19:25:47 +0800

    Updated copyright year

--- interpretablefa/interpretablefa.py
+++ interpretablefa/interpretablefa.py
@@ -1,4 +1,4 @@
-# Copyright 2025 Justin Philip Tuazon, Gia Mizrane Abubo
+# Copyright 2025-2026 Justin Philip Tuazon, Gia Mizrane Abubo
 
 # This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
 # License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
